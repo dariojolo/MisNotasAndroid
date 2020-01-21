873d8f8df1cef1b193235e096d955f6066e33ad1
Dario <dariojolo@gmail.com>
2020-01-21 17:46:17 -0300

    Agregado segundo RecyclerView
    Inicio Agregado bottom bar and RecyclerView

---
+++ app/src/main/java/ar/com/dariojolo/misnotas/MyAdapter.java
@@ -0,0 +1,70 @@
+package ar.com.dariojolo.misnotas;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyHolder> {
+
+    Context context;
+    ArrayList<CardModel>listado; //Lista donde se va a guardar la info para mostrar en el RecyclerView
+
+    public MyAdapter(Context context, ArrayList<CardModel> listado) {
+        this.context = context;
+        this.listado = listado;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_element,null); //Infla el elemento del recyclerview a cargar
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MyHolder holder, int position) {
+        holder.mTitle.setText(listado.get(position).getTitle());
+        holder.mDescription.setText(listado.get(position).getDescription());
+        holder.mImage.setImageResource(listado.get(position).getImg());
+
+        holder.setItemClickListener(new ItemClickListener() {
+            @Override
+            public void onItemClickListener(View view, int position) {
+                String gTitle = listado.get(position).getTitle();
+                String gDesc = listado.get(position).getDescription();
+                BitmapDrawable bitmapDrawable = (BitmapDrawable)holder.mImage.getDrawable();
+
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+
+                byte[]bytes = stream.toByteArray();
+
+                Intent intent = new Intent(context, DetailActivity.class);
+
+                intent.putExtra("iTitle", gTitle);
+                intent.putExtra("iDesc", gDesc);
+                intent.putExtra("iImage", bytes);
+
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return listado.size();
+    }
+}
